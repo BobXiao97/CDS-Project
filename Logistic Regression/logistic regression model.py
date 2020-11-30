4a3fef1329b0c418dc8c837f26a0f9a3091771b5
BobXiao97 <xtq1997@gmail.com>
2020-11-30 20:18:40 +0800

    LR is underfitting for this problem

--- Logistic Regression/logistic regression model.py
+++ Logistic Regression/logistic regression model.py
@@ -1,24 +1,26 @@
 import pandas as pd 
 import numpy as np
 from sklearn.model_selection import train_test_split
-
-data=pd.read_csv('labeled_spotify_data_genre_clean.csv')
+import random
+data=pd.read_csv('labeled_spotify_data_genre_clean_balanced.csv')
 x=data.drop(columns=['Label'])
 x=x.astype('float')
-x['duration_s']=x['duration_ms']/1000
-x=x.drop(columns='duration_ms')
+x=(x-x.min())/(x.max()-x.min())
 y=data['Label']
+
 
 bias=[]
 for i in range(0,data.shape[0]):
     bias.append(1)
 x['bias']=bias
 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.33,random_state=0)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
 
 theta=[]
+theta_temp=[]
 for i in range(0,14):
-    theta.append(0)
+    theta.append(1)
+
 
 
 def sigmoid(theta,x):
@@ -36,24 +38,76 @@ def gradient_descent(eta,x,y,theta):
     result=theta-third
     return result
 
-
-
-for j in range(0,x_train.shape[0]):
-    theta=gradient_descent(0.00001,x_train.iloc[j],y_train.iloc[j],theta)
+for k in range(0,100000):
+    j=random.randint(0,x_train.shape[0]-1)
+    theta=gradient_descent(0.0001,x_train.iloc[j],y_train.iloc[j],theta)
 print(theta)
 
-correct=0
+TP=0
+TN=0
+FP=0
+FN=0
 total=0
+
 for i in range(0,x_test.shape[0]):
     result=sigmoid(theta,x_test.iloc[i])
     if result>=0.5:
         result=1
     else:
         result=0
-    if result==y_test.iloc[i]:
-        correct+=1
+    if result==1 and result==y_test.iloc[i]:
+        TP+=1
+    if result==0 and result==y_test.iloc[i]:
+        TN+=1
+    if result==1 and result!=y_test.iloc[i]:
+        FP+=1
+    if result==0 and result!=y_test.iloc[i]:
+        FN+=1
     total+=1
 
-print(correct/total)
+precision=TP/(TP+FP)
+recall=TP/(TP+FN)
+print('test set:')
+print('accuracy:')
+print((TP+TN)/total)
+print('precision:')
+print(precision)
+print('recall:')
+print(recall)
+print('F1')
+print(2*precision*recall/(precision+recall))
         
+TP=0
+TN=0
+FP=0
+FN=0
+total=0
+
+for i in range(0,x_train.shape[0]):
+    result=sigmoid(theta,x_train.iloc[i])
+    if result>=0.5:
+        result=1
+    else:
+        result=0
+    if result==1 and result==y_train.iloc[i]:
+        TP+=1
+    if result==0 and result==y_train.iloc[i]:
+        TN+=1
+    if result==1 and result!=y_train.iloc[i]:
+        FP+=1
+    if result==0 and result!=y_train.iloc[i]:
+        FN+=1
+    total+=1
+
+precision=TP/(TP+FP)
+recall=TP/(TP+FN)
+print('training set:')
+print('accuracy:')
+print((TP+TN)/total)
+print('precision:')
+print(precision)
+print('recall:')
+print(recall)
+print('F1')
+print(2*precision*recall/(precision+recall))
 
