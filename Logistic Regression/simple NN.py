0b46470064114162acd1e137317c78fff8c9bccb
BobXiao97 <xtq1997@gmail.com>
2020-12-06 20:00:43 +0800

    Simple neural network

---
+++ Logistic Regression/simple NN.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from keras.layers import Dense,Dropout,Flatten
+from keras.models import Sequential
+data=pd.read_csv('data_w_genre_balanced.csv')
+x=data.drop(columns=['Label'])
+x=x.astype('float')
+x=(x-x.min())/(x.max()-x.min())
+y=data['Label']
+
+bias=[]
+for i in range(0,data.shape[0]):
+    bias.append(1)
+x['bias']=bias
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+
+
+model=Sequential()
+model.add(Dense(units=128,activation='elu',input_shape=(x_train.shape[1],)))
+model.add(Dropout(0.2))
+model.add(Dense(units=256,activation='elu'))
+model.add(Dropout(0.2))
+model.add(Dense(units=512,activation='elu'))
+model.add(Dropout(0.2))
+model.add(Dense(units=256,activation='elu'))
+model.add(Dropout(0.2))
+model.add(Dense(units=128,activation='elu'))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dropout(0.2))
+model.add(Dense(1,activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+print('Train...')
+model.fit(x_train,y_train,batch_size=20,epochs=50,validation_split=0.1)
+score,acc=model.evaluate(x_test, y_test,batch_size=10)
+print('Test score:',score)
+print('Test accuracy:',acc)
